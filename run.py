ae285dde9cf2d92ba897f884922fa3253ce282ee
Luyang Chen <LouisChen1992@hotmail.com>
2019-01-30 18:44:33 -0800

    IPCA model

--- run.py
+++ run.py
@@ -1,27 +1,78 @@
-import matplotlib.pyplot as plt
+import json
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
+from src.utils import deco_print
 from src.data.data_layer import DataInRamInputLayer
+from src.model.model_IPCA_naive import ModelIPCA_Naive
 from src.model.model_IPCA_FFN import ModelIPCA_FFN
 
-dl_train = DataInRamInputLayer('datasets/CharAll_na_rm_all_50_percent_train.npz')
-dl_valid = DataInRamInputLayer('datasets/CharAll_na_rm_all_50_percent_valid.npz')
-dl_test = DataInRamInputLayer('datasets/CharAll_na_rm_all_50_percent_test.npz')
+tf.flags.DEFINE_string('config', '', 'Path to the file with configurations')
+tf.flags.DEFINE_boolean('evalStats', True, 'Calculate statistics')
 
-model = ModelIPCA_FFN(46, 240, [], 4, 0.1, 1.0, '.', dl_train)
+FLAGS = tf.flags.FLAGS
 
-sess = tf.Session()
+def main(_):
+	with open(FLAGS.config, 'r') as file:
+		config = json.load(file)
+	deco_print('Read the following in config: ')
+	print(json.dumps(config, indent=4))
 
-sess.run(tf.global_variables_initializer())
+	deco_print('Creating data layer')
+	dl_train = DataInRamInputLayer(config['individual_feature_file'])
+	dl_valid = DataInRamInputLayer(config['individual_feature_file_valid'])
+	dl_test = DataInRamInputLayer(config['individual_feature_file_test'])
+	deco_print('Data layer created')
 
-F = np.random.randn(240, 4)
+	model = ModelIPCA_FFN(individual_feature_dim=config['individual_feature_dim'], 
+						tSize=config['tSize_train'], 
+						hidden_dims=config['hidden_dims'], 
+						nFactor=config['nFactor'], 
+						lr=config['lr'], 
+						dropout=config['dropout'],
+						logdir=config['logdir'], 
+						dl=dl_train, 
+						is_train=True)
+	gpu_options = tf.GPUOptions(allow_growth=True)
+	sess_config = tf.ConfigProto(gpu_options=gpu_options)
+	sess = tf.Session(config=sess_config)
+	model.randomInitialization(sess)
 
-loss_list = []
-for _ in range(10):
-    loss = model._step_parameters(sess, F_data=F, maxIter=8)
-    loss_list.append(loss)
-    F = model._step_factor(sess)
+	model_naive = ModelIPCA_Naive(46, 'model/IPCA_naive')
+	model_naive.loadSavedModel(config['nFactor'])
+	initial_F = model_naive.getFactors(dl_train)
 
-plt.plot(np.concatenate(loss_list))
-plt.show()
+	loss_epoch_list = model.train(sess, initial_F=initial_F)
+
+	if FLAGS.evalStats:
+		model_valid = ModelIPCA_FFN(individual_feature_dim=config['individual_feature_dim'], 
+								tSize=config['tSize_valid'], 
+								hidden_dims=config['hidden_dims'], 
+								nFactor=config['nFactor'], 
+								lr=config['lr'], 
+								dropout=config['dropout'],
+								logdir=config['logdir'], 
+								dl=dl_valid, 
+								is_train=False, 
+								force_var_reuse=True)
+		model_test = ModelIPCA_FFN(individual_feature_dim=config['individual_feature_dim'], 
+								tSize=config['tSize_test'], 
+								hidden_dims=config['hidden_dims'], 
+								nFactor=config['nFactor'], 
+								lr=config['lr'], 
+								dropout=config['dropout'],
+								logdir=config['logdir'], 
+								dl=dl_test, 
+								is_train=False, 
+								force_var_reuse=True)
+		model.loadSavedModel(sess)
+		w = model.getMarkowitzWeight(sess)
+		stats = pd.DataFrame(np.zeros((4,3)), columns=['train', 'valid', 'test'], index=['SR', 'UV', 'Alpha', 'Alpha_weighted'])
+		stats.loc[:,'train'] = model.calculateStatistics(sess, w)
+		stats.loc[:,'valid'] = model_valid.calculateStatistics(sess, w)
+		stats.loc[:,'test'] = model_test.calculateStatistics(sess, w)
+		print(stats)
+
+if __name__ == '__main__':
+	tf.app.run()
--- src/model/model_IPCA_FFN.py
+++ src/model/model_IPCA_FFN.py
@@ -1,9 +1,13 @@
 import os
+import time
 import numpy as np
 import tensorflow as tf
 
 from src.utils import deco_print
 from src.utils import sharpe
+from src.utils import decomposeReturn
+from src.utils import UnexplainedVariation
+from src.utils import FamaMcBethAlpha
 
 class ModelIPCA_FFN:
 	def __init__(self, 
@@ -15,6 +19,7 @@ class ModelIPCA_FFN:
 				dropout,
 				logdir, 
 				dl, 
+				is_train=False,
 				force_var_reuse=False):
 		self._individual_feature_dim = individual_feature_dim
 		self._tSize = tSize
@@ -23,18 +28,22 @@ class ModelIPCA_FFN:
 		self._lr = lr
 		self._dropout = dropout
 		self._logdir = logdir
+		self._logdir_nFactor = os.path.join(self._logdir, str(self._nFactor))
+		self._is_train = is_train
 		self._force_var_reuse = force_var_reuse
 		
 		self._load_data(dl)
 		self._build_placeholder()
 		with tf.variable_scope('Model_Layer', reuse=self._force_var_reuse):
 			self._build_forward_pass_graph()
-		self._build_train_op()
+		if self._is_train:
+			self._build_train_op()
 
 	def _load_data(self, dl):
 		for _, (I_macro, I, R, mask) in enumerate(dl.iterateOneEpoch(subEpoch=False)):
 			self._I_data = I[mask]
 			self._R_data = R[mask]
+			self._mask_data = mask
 			self._splits_data = mask.sum(axis=1)
 			self._splits_np_data = self._splits_data.cumsum()[:-1]
 			self._R_list_data = np.split(self._R_data, self._splits_np_data)
@@ -79,38 +88,93 @@ class ModelIPCA_FFN:
 		beta, = sess.run(fetches=[self._beta], feed_dict=feed_dict)
 		return beta
 
-	def _step_factor(self, sess):
+	def getMarkowitzWeight(self, sess):
+		F, _ = self._step_factor(sess)
+		w = self._Markowitz(F)
+		return w
+
+	def getFactors(self, sess, calculate_residual=True):
+		F, residual_list = self._step_factor(sess, calculate_residual=calculate_residual)
+		if calculate_residual:
+			residual = np.zeros_like(self._mask_data, dtype=float)
+			residual[self._mask_data] = np.squeeze(np.concatenate(residual_list))
+			return F, residual
+		else:
+			return F, None
+
+	def getSDFFactor(self, sess, w):
+		F, _ = self._step_factor(sess)
+		return F.dot(w)
+
+	def _Markowitz(self, r):
+		Sigma = r.T.dot(r) / r.shape[0]
+		mu = np.mean(r, axis=0)
+		w = np.dot(np.linalg.pinv(Sigma), mu)
+		return w
+
+	def _step_factor(self, sess, calculate_residual=False):
 		beta = self.getBeta(sess)
 		beta_list = np.split(beta, self._splits_np_data)
 		F_list = []
+		if calculate_residual:
+			residual_list = []
 		for R_t, beta_t in zip(self._R_list_data, beta_list):
 			F_t = np.linalg.pinv(beta_t.T.dot(beta_t)).dot(beta_t.T.dot(R_t))
-			F_list.append(F_list)
-		return np.array(F_list)
+			F_list.append(F_t)
+			if calculate_residual:
+				residual_list.append(R_t - beta_t.dot(F_t))
+		if calculate_residual:
+			return np.array(F_list), residual_list
+		else:
+			return np.array(F_list), None
 
-	def _step_parameters(self, sess, F_data, maxIter=1024, tol=1e-06):
-		old_variables = self.getParameters(sess)
-		nIter = 0
-		success = False
-		loss_list = []
-		while nIter < maxIter:
-			feed_dict = {self._I_placeholder:self._I_data,
+	def _step_parameters(self, sess, F_data, maxIter=1024, tol=1e-06, eval_loss=True):
+		feed_dict_train = {self._I_placeholder:self._I_data,
 						self._R_placeholder:self._R_data,
 						self._F_placeholder:F_data,
 						self._splits_placeholder:self._splits_data,
 						self._dropout_placeholder:self._dropout}
-			_, loss = sess.run(fetches=[self._train_op, self._loss], feed_dict=feed_dict)
-			loss_list.append(loss)
-			new_variables = self.getParameters(sess)
-			nIter += 1
-			if self._max_norm_difference(old_variables, new_variables) < tol:
+		
+		feed_dict_eval = {self._I_placeholder:self._I_data,
+						self._R_placeholder:self._R_data,
+						self._F_placeholder:F_data,
+						self._splits_placeholder:self._splits_data,
+						self._dropout_placeholder:1.0}
+
+		if eval_loss:
+			old_loss, = sess.run(fetches=[self._loss], feed_dict=feed_dict_eval)
+		else:
+			old_variables = self.getParameters(sess)
+
+		success = False
+		loss_list = []
+		error_list = []
+
+		for _ in range(maxIter):
+			sess.run(fetches=[self._train_op], feed_dict=feed_dict_train)
+			new_loss, = sess.run(fetches=[self._loss], feed_dict=feed_dict_eval)
+			loss_list.append(new_loss)
+
+			if eval_loss:
+				error = abs(new_loss - old_loss) / max(abs(old_loss), 1e-08)
+				error_list.append(error)
+				old_loss = new_loss
+			else:
+				new_variables = self.getParameters(sess)
+				error = self._max_norm_difference(old_variables, new_variables)
+				error_list.append(error)
+				old_variables = new_variables
+
+			if error < tol:
 				success = True
 				break
+
 		if success:
 			deco_print('Converged! ')
 		else:
 			deco_print('WARNING: Exceed maximum number of iterations! ')
-		return loss_list
+
+		return loss_list, error_list
 
 	def _max_norm_difference(self, v1_list, v2_list):
 		tmp = 0.0
@@ -121,3 +185,70 @@ class ModelIPCA_FFN:
 	def getParameters(self, sess):
 		trainable_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Model_Layer')
 		return sess.run(trainable_variables)
+
+	def evalLoss(self, sess, F_data):
+		feed_dict_eval = {self._I_placeholder:self._I_data,
+						self._R_placeholder:self._R_data,
+						self._F_placeholder:F_data,
+						self._splits_placeholder:self._splits_data,
+						self._dropout_placeholder:1.0}
+		loss, = sess.run(fetches=[self._loss], feed_dict=feed_dict_eval)
+		return loss
+
+	def randomInitialization(self, sess):
+		sess.run(tf.global_variables_initializer())
+		deco_print('Random initialization')
+
+	def loadSavedModel(self, sess):
+		if tf.train.latest_checkpoint(self._logdir_nFactor) is not None:
+			saver = tf.train.Saver(max_to_keep=128)
+			saver.restore(sess, tf.train.latest_checkpoint(self._logdir_nFactor))
+			deco_print('Restored checkpoint')
+		else:
+			deco_print('WARNING: Checkpoint not found! Use random initialization! ')
+			self.randomInitialization(sess)
+
+	def train(self, sess, initial_F=None, numEpoch=128, 
+		maxIter=2048, tol=1e-06):
+		if initial_F is None:
+			F = np.random.randn(self._tSize, self._nFactor)
+		else:
+			F = initial_F
+
+		saver = tf.train.Saver(max_to_keep=128)
+		if os.path.exists(self._logdir_nFactor):
+			os.system('rm -rf %s' %self._logdir_nFactor)
+
+		old_loss = self.evalLoss(sess, F)
+		best_loss = float('inf')
+		loss_epoch_list = [old_loss]
+
+		time_start = time.time()
+		for epoch in range(numEpoch):
+			deco_print('Doing Epoch %d' %epoch)
+			_, _ = self._step_parameters(sess, F_data=F, maxIter=maxIter, tol=tol)
+			F, _ = self._step_factor(sess)
+			new_loss = self.evalLoss(sess, F)
+			loss_epoch_list.append(new_loss)
+
+			if new_loss < best_loss:
+				best_loss = new_loss
+				deco_print('Saving current best checkpoint')
+				saver.save(sess, save_path=os.path.join(self._logdir_nFactor, 'model-best'))
+			time_elapse = time.time() - time_start
+			time_est = time_elapse / (epoch+1) * numEpoch
+			deco_print('Epoch %d Loss: %0.4f' %new_loss)
+			deco_print('Epoch %d Elapse/Estimate: %0.2fs/%0.2fs' %(epoch, time_elapse, time_est))
+			print('\n')
+
+		return loss_epoch_list
+
+	def calculateStatistics(self, sess, w):
+		SR = sharpe(self.getSDFFactor(sess, w))
+		_, residual = self.getFactors(sess, calculate_residual=True)
+		R = np.zeros_like(self._mask_data, dtype=float)
+		R[self._mask_data] = self._R_data
+		UV = UnexplainedVariation(R, residual, self._mask_data)
+		Alpha = FamaMcBethAlpha(residual, self._mask_data, weighted=False)
+		Alpha_weighted = FamaMcBethAlpha(residual, self._mask_data, weighted=True)
+		return (SR, UV, Alpha, Alpha_weighted)
